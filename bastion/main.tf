6a9e51e9d0e965ecd27c348b165eb2aa22fc7f5f
David Lopez <dlopezza@unal.edu.co>
2024-10-20 22:40:33 -0500

    fix: remove github ip adresses from bastion sg, TBD

--- bastion/main.tf
+++ bastion/main.tf
@@ -17,14 +17,11 @@ resource "aws_security_group" "github_actions_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  dynamic "ingress" {
-    for_each = split("\n", trimspace(data.local_file.github_actions_ips.content))
-    content {
+  ingress {
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
-      cidr_blocks = [trimspace(ingress.value)]
-    }
+      cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
