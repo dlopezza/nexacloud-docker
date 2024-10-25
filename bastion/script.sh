acfde4a588b32e5094e8c47f7409451770c009e8
David Lopez <dlopezza@unal.edu.co>
2024-10-24 21:54:36 -0500

    update bastion

--- bastion/script.sh
+++ bastion/script.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 exec > /var/log/user-data.log 2>&1
-echo "Starting Terraform installation"
 sudo yum install -y yum-utils shadow-utils
-echo "Adding HashiCorp repository"
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-echo "Installing Terraform"
 sudo yum -y install terraform
-echo "Terraform installation complete"
 terraform -version
 EOF
