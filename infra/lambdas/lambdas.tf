35e8b2627a1a59360e87a404f11ca78167720b62
David Lopez <dlopezza@unal.edu.co>
2024-10-26 23:12:55 -0500

    fix: link usage plan with api gateaway and key

--- infra/lambdas/lambdas.tf
+++ infra/lambdas/lambdas.tf
@@ -1,60 +1,55 @@
-resource "aws_lambda_function" "this" {
-  filename      = var.filename
-  function_name = var.function_name
-  role          = var.role
-  handler       = var.handler
-  runtime       = var.runtime
+resource "aws_api_gateway_rest_api" "LambdasApi" {
+  name        = "LambdasApi"
+  description = "API Gateway for lambdas"
+}
 
-  environment {
-    variables = var.environment_variables
+resource "aws_api_gateway_api_key" "api_key" {
+  name = "nexa-api-key"
+}
+
+resource "aws_api_gateway_usage_plan" "usage_plan" {
+  name = "nexa-usage-plan"
+  
+  api_stages {
+    api_id = aws_api_gateway_rest_api.LambdasApi.id
+    stage  = "default"
   }
-
-  dynamic "vpc_config" {
-    for_each = var.vpc_config != null ? [1] : []
-    content {
-      subnet_ids         = var.vpc_config.subnet_ids
-      security_group_ids = var.vpc_config.security_group_ids
-    }
+  
+  quota_settings {
+    limit  = 1000
+    period = "MONTH"
+  }
+  
+  throttle_settings {
+    burst_limit = 100
+    rate_limit  = 50
   }
 }
 
-resource "aws_api_gateway_resource" "this" {
-  rest_api_id = aws_api_gateway_rest_api.LambdasApi.id
-  parent_id   = aws_api_gateway_rest_api.LambdasApi.root_resource_id
-  path_part   = var.path
-}
-
-resource "aws_api_gateway_method" "this" {
-  rest_api_id   = aws_api_gateway_rest_api.LambdasApi.id
-  resource_id   = aws_api_gateway_resource.this.id
-  http_method   = var.httpMethod
-  authorization = "NONE" 
-  api_key_required = true 
-}
-
-resource "aws_api_gateway_integration" "this" {
-  rest_api_id             = aws_api_gateway_rest_api.LambdasApi.id
-  resource_id             = aws_api_gateway_resource.this.id
-  http_method             = aws_api_gateway_method.this.http_method
-  integration_http_method = "POST"  # POST for Lambda
-  type                    = "AWS_PROXY"  # AWS_PROXY integration
-
-  uri = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.this.arn}/invocations"
-}
-
-resource "aws_lambda_permission" "allow_apigateway" {
-  statement_id  = "AllowExecutionFromAPIGateway-${var.function_name}"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.this.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.LambdasApi.execution_arn}/*/*"  # Allow all methods and resources
+resource "aws_api_gateway_usage_plan_key" "usage_plan_key" {
+  key_id        = aws_api_gateway_api_key.api_key.id
+  key_type      = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.usage_plan.id
 }
 
 resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.LambdasApi.id
-  stage_name  = "default"  # Single deployment stage
-  
+  stage_name  = "default"
+
   triggers = {
-    redeployment = sha1(jsonencode([aws_api_gateway_integration.this]))
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_integration.this,
+      aws_api_gateway_method.this
+    ]))
   }
+
+  depends_on = [
+    aws_api_gateway_method.this,
+    aws_api_gateway_integration.this
+  ]
+}
+
+output "api_key_value" {
+  value     = aws_api_gateway_api_key.api_key.value
+  sensitive = true
 }
