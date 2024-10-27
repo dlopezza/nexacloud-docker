c853d8417049124e9fb11031dc3301feffb6f1cd
David Lopez <dlopezza@unal.edu.co>
2024-10-27 18:34:47 -0500

    fix: add dependencies to api gateway usage plan

--- infra/lambdas/apiGateway.tf
+++ infra/lambdas/apiGateway.tf
@@ -11,11 +11,12 @@ resource "aws_api_gateway_stage" "default" {
   deployment_id = aws_api_gateway_deployment.this.id
   rest_api_id   = aws_api_gateway_rest_api.LambdasApi.id
   stage_name    = "default"
+  depends_on    = []
 }
 
 resource "aws_api_gateway_usage_plan" "usage_plan" {
   name = "nexa-usage-plan"
-  depends_on = [aws_api_gateway_deployment.this]
+  depends_on = [aws_api_gateway_deployment.this, aws_api_gateway_stage.default]
 
   api_stages {
     api_id = aws_api_gateway_rest_api.LambdasApi.id
@@ -37,4 +38,5 @@ resource "aws_api_gateway_usage_plan_key" "usage_plan_key" {
   key_id        = aws_api_gateway_api_key.api_key.id
   key_type      = "API_KEY"
   usage_plan_id = aws_api_gateway_usage_plan.usage_plan.id
+  depends_on    = [aws_api_gateway_stage.default, aws_api_gateway_usage_plan.usage_plan]
 }
