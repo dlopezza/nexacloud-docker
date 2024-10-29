1bb5d7b46ee33c961415707ea315d42054fde969
David Lopez <dlopezza@unal.edu.co>
2024-10-29 00:33:49 -0500

    fix: change api gateway output to reflect environment

--- infra/lambdas/outputs.tf
+++ infra/lambdas/outputs.tf
@@ -1,5 +1,5 @@
 output "api_gateway_url" {
-  value       = "https://${aws_api_gateway_rest_api.LambdasApi.id}.execute-api.us-east-1.amazonaws.com/default/${var.path}"
+  value       = "https://${aws_api_gateway_rest_api.LambdasApi.id}.execute-api.us-east-1.amazonaws.com/${var.environment}/${var.path}"
   description = "API Gateway URL for the Lambda function"
 }
 
