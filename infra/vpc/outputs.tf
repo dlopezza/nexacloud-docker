26a13418cad8bd5877a8db8bb341e196e53d9583
David Lopez <dlopezza@unal.edu.co>
2024-11-03 00:03:27 -0500

    fix: rework output structure for subnet ids

--- infra/vpc/outputs.tf
+++ infra/vpc/outputs.tf
@@ -5,10 +5,10 @@ output vpc_id {
 
 output "public_subnets" {
   description = "List of public subnet IDs"
-  value       = module.public_subnets.subnet_ids
+  value       = [for subnet in module.public_subnets : subnet.subnet_id]
 }
 
 output "private_subnets" {
   description = "List of private subnet IDs"
-  value       = module.private_subnets.subnet_ids
+  value       = [for subnet in module.private_subnets : subnet.subnet_id]
 }
--- infra/vpc/subnet/outputs.tf
+++ infra/vpc/subnet/outputs.tf
@@ -1,4 +1,4 @@
-output "subnet_ids" {
-  description = "List of subnet IDs"
-  value       = [for subnet in aws_subnet.this : subnet.id]
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = aws_subnet.this.id
 }
