43b430461610e0233f507c54e342a408fb3543d2
David Lopez <dlopezza@unal.edu.co>
2024-11-03 21:21:07 -0500

    fix: add docker to bastion instalation script

--- bastion/script.sh
+++ bastion/script.sh
@@ -4,4 +4,8 @@ sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 terraform -version
+
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
 EOF
