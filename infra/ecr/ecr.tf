5478fa235b01dffb69484a1ab23ec24b583a14a6
David Lopez <dlopezza@unal.edu.co>
2024-11-03 23:25:26 -0500

    fix: move provider to ecr file

--- infra/ecr/ecr.tf
+++ infra/ecr/ecr.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"  # Specify the version as needed
+    }
+  }
+}
+
+provider "docker" {
+  registry_auth {
+    address  = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
+    username = data.aws_ecr_authorization_token.token.user_name
+    password = data.aws_ecr_authorization_token.token.password
+  }
+}
+
 data "aws_caller_identity" "current" {}
 data "aws_ecr_authorization_token" "token" {}
 
