617241bdb0b71ad96c6e38d319204700824ac200
David Lopez <dlopezza@unal.edu.co>
2024-11-03 22:52:36 -0500

    fix: move provider to dev main.tf file

--- infra/ecr/ecr.tf
+++ infra/ecr/ecr.tf
@@ -1,10 +1,3 @@
-provider "docker" {
-  registry_auth {
-    address  = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
-    username = data.aws_ecr_authorization_token.token.user_name
-    password = data.aws_ecr_authorization_token.token.password
-  }
-}
 data "aws_caller_identity" "current" {}
 data "aws_ecr_authorization_token" "token" {}
 
--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -27,6 +27,14 @@ provider "aws" {
   }
 }
 
+provider "docker" {
+  registry_auth {
+    address  = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
+    username = data.aws_ecr_authorization_token.token.user_name
+    password = data.aws_ecr_authorization_token.token.password
+  }
+}
+
 module "vpc" {
   source = "../../vpc"
   environment                = var.environment
