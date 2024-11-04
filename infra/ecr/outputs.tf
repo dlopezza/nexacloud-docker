c45f233feabb8f446b6f614c9302fb8142c544af
David Lopez <dlopezza@unal.edu.co>
2024-11-03 23:32:32 -0500

    fix: fix reopository url in ecr output

--- infra/ecr/outputs.tf
+++ infra/ecr/outputs.tf
@@ -1,4 +1,4 @@
 output "repository_url" {
   description = "The URL of the ECR repository"
-  value       = aws_ecr_repository.repository.repository_url
+  value       = aws_ecr_repository.this.repository_url
 }
