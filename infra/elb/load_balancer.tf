9a96809145f5b04a46667ca3a27cf156504231e2
David Lopez <dlopezza@unal.edu.co>
2024-11-10 20:15:32 -0500

    fix: modify health check for elb

--- infra/elb/load_balancer.tf
+++ infra/elb/load_balancer.tf
@@ -32,7 +32,15 @@ resource "aws_lb_target_group" "this" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   target_type = "instance"
-  health_check_type = "EC2"
+
+  health_check {
+    path                = "/index.html"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb_listener" "this" {
