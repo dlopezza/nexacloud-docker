9ba782603924f5a8f103785faabdbdf765411b03
David Lopez <dlopezza@unal.edu.co>
2024-11-10 20:13:04 -0500

    fix: modify heath check type in autoscaling group

--- infra/elb/load_balancer.tf
+++ infra/elb/load_balancer.tf
@@ -32,15 +32,7 @@ resource "aws_lb_target_group" "this" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   target_type = "instance"
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 3
-    unhealthy_threshold = 2
-  }
+  health_check_type = "EC2"
 }
 
 resource "aws_lb_listener" "this" {
